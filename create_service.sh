3567a5a34e6399a9f73141fd69cf9d56f9a12e06
Satender Saroha <ssaroha@yahoo.com>
2012-06-24 11:41:26 +0530

    first commit with maven pom.xml to produce a war file which can be simply dropped into a tomcat webapp folder.

---
+++ create_service.sh
@@ -0,0 +1,9 @@
+mvn archetype:generate \
+    -DarchetypeGroupId=org.apache.maven.archetypes \
+    -DarchetypeArtifactId=maven-archetype-webapp \
+    -DgroupId=com.mykidscart.mahout_service \
+    -DartifactId=mahout_service \
+    -Dpackage=com.mykidscart.mahout_service \
+    -Dversion=1.0.0 \
+    -DjarName=mahout_service \
+    -Dsource=1.6 \
